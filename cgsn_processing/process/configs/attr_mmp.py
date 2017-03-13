efc58eebd4f6e4f2695aa5d4abb425dbc223317b
Joe Futrelle <joefutrelle@gmail.com>
2017-03-13 15:46:00 +0000

    using caps/space format for long names

--- cgsn_processing/process/configs/attr_mmp.py
+++ cgsn_processing/process/configs/attr_mmp.py
@@ -9,26 +9,30 @@
 
 MMP = {
     'z': {
-        'long_name': 'depth',
+        'long_name': 'Depth',
         'standard_name': 'depth_of_sensor_below_water',
         'units': 'm',
         'positive': 'down',
         'axis': 'Z',
+        'valid_min': '-10000',
+        'valid_max': '1000',
     },
     'depth': {
-        'long_name': 'depth',
+        'long_name': 'Sensor Depth',
         'standard_name': 'depth_of_sensor_below_water',
         'units': 'm',
         'positive': 'down',
         'axis': 'Z',
+        'valid_min': '-10000',
+        'valid_max': '1000',
     },
     'pressure': {
-        'long_name': 'sea pressure',
+        'long_name': 'Sea Water Pressure',
         'standard_name': 'sea_water_pressure_due_to_sea_water',
         'units': 'mbar'
     },
     'temperature': {
-        'long_name': 'temperature',
+        'long_name': 'Sea Water Temperature',
         'standard_name': 'sea_water_temperature',
         'units': 'degree_Celsius'
     }
