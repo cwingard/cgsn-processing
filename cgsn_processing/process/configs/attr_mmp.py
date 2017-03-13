d964622c1eeb085ae9e08988a92c8c85abcbe02f
Joe Futrelle <joefutrelle@gmail.com>
2017-03-13 14:48:39 +0000

    computing depth, adding attrs, fixing attr handling

--- cgsn_processing/process/configs/attr_mmp.py
+++ cgsn_processing/process/configs/attr_mmp.py
@@ -6,6 +6,28 @@
 @author Joe Futrelle
 @brief Attributes for the MMP variables
 """
+MMP = {
+    'z': {
+        'long_name': 'depth',
+        'standard_name': 'depth_of_sensor_below_water',
+        'units': 'm',
+        'positive': 'down',
+        'axis': 'Z',
+    },
+    'depth': {
+        'long_name': 'depth',
+        'standard_name': 'depth_of_sensor_below_water',
+        'units': 'm',
+        'positive': 'down',
+        'axis': 'Z',
+    },
+    'pressure': {
+        'long_name': 'sea pressure',
+        'standard_name': 'sea_water_pressure_due_to_sea_water',
+        'units': 'mbar'
+    }
+}
+
 MMP_ADATA = {
 }
 
--- cgsn_processing/process/proc_mmp.py
+++ cgsn_processing/process/proc_mmp.py
@@ -13,14 +13,16 @@ import datetime
 import numpy as np
 import pandas as pd
 
-from pocean.dsg.timeseries.om import OrthogonalMultidimensionalTimeseries
+from pocean.utils import dict_update
+from pocean.dsg.timeseries.om import OrthogonalMultidimensionalTimeseries as OMTs
+from gsw import z_from_p
 
 from cgsn_processing.process.common import inputs
-from cgsn_processing.process.configs.attr_mmp import MMP_ADATA, MMP_CDATA, MMP_EDATA
+from cgsn_processing.process.configs.attr_mmp import MMP, MMP_ADATA, MMP_CDATA, MMP_EDATA
 
 from pocean.dsg.timeseries.om import OrthogonalMultidimensionalTimeseries
 
-def json2dataframes(j):
+def json2dataframes(j, lat=0.):
     # split adata "beams" into multiple columns
     adata = j['adata']
     adata['beams1'] = [b[1] for b in adata['beams']]
@@ -52,6 +54,14 @@ def json2dataframes(j):
     ipressure = pressure.interpolate(method='time')[np.isnan(pressure)]
     adata['pressure'] = ipressure
 
+    # convert pressure to depth
+    def compute_depth(df):
+        df['depth'] = [0 - z_from_p(p, lat) for p in df['pressure']]
+
+    compute_depth(adata)
+    compute_depth(cdata)
+    compute_depth(edata)
+                       
     # now store time indices in "time" columns
     adata['time'] = adata.index
     cdata['time'] = cdata.index
@@ -62,21 +72,6 @@ def json2dataframes(j):
         'cdata': cdata,
         'edata': edata
     }
-
-class ADataTimeseries(OrthogonalMultidimensionalTimeseries):
-    def nc_attributes(self):
-        attrs = super(ADataTimeseries, self).nc_attributes()
-        return dict_update(attrs, MMP_ADATA)
-    
-class CDataTimeseries(OrthogonalMultidimensionalTimeseries):
-    def nc_attributes(self):
-        attrs = super(CDataTimeseries, self).nc_attributes()
-        return dict_update(attrs, MMP_CDATA)
-    
-class EDataTimeseries(OrthogonalMultidimensionalTimeseries):
-    def nc_attributes(self):
-        attrs = super(EDataTimeseries, self).nc_attributes()
-        return dict_update(attrs, MMP_EDATA)
     
 def json2netcdf(json_path, out_basepath, lat=0., lon=0.):
     out_basename, _ = os.path.splitext(out_basepath)
@@ -89,26 +84,31 @@ def json2netcdf(json_path, out_basepath, lat=0., lon=0.):
     def massage_dataframe(df):
         df['y'] = lat
         df['x'] = lon
-        df['z'] = 0. # FIXME compute depth
-        df['station'] = 0 # FIXME use profile ID?
+        # in the timeseries representation, there's one z per station
+        # this will be fixed when using the profile representation
+        df['z'] =  j['profile']['start_depth']
+        df['station'] = 0
         df['t'] = df.pop('time')
         # convert all int64s to int32s
         for col in df.columns:
             if df[col].dtype == np.int64:
                 df[col] = df[col].astype(np.int32)
         return df
-
+    
     adf = massage_dataframe(dfs['adata'])
     out_path = '{}-adata.nc'.format(out_basepath)
-    ADataTimeseries.from_dataframe(adf, out_path)
+    attrs = dict_update(MMP, MMP_ADATA)
+    OMTs.from_dataframe(adf, out_path, attributes=attrs)
 
     cdf = massage_dataframe(dfs['cdata'])
     out_path = '{}-cdata.nc'.format(out_basepath)
-    CDataTimeseries.from_dataframe(cdf, out_path)
+    attrs = dict_update(MMP, MMP_CDATA)
+    OMTs.from_dataframe(cdf, out_path, attributes=attrs)
 
     edf = massage_dataframe(dfs['edata'])
     out_path = '{}-edata.nc'.format(out_basepath)
-    EDataTimeseries.from_dataframe(edf, out_path)
+    attrs = dict_update(MMP, MMP_EDATA)
+    OMTs.from_dataframe(edf, out_path, attributes=attrs)
 
 def main():
     # load  the input arguments
