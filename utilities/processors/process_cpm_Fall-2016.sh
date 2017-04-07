a93e2395e2d1967a34ac5db24c93f4c9b692dad1
Christopher Wingard <cwingard@coas.oregonstate.edu>
2017-04-07 20:45:44 +0000

    Adds the STC supervisor data

--- utilities/processors/process_cpm_Fall-2016.sh
+++ utilities/processors/process_cpm_Fall-2016.sh
@@ -1,8 +1,8 @@
 #!/bin/bash -e
 #
-# Parse the various data files for a Coastal Surface Mooring.
+# Parse the various data files for a Coastal Profiler Mooring.
 #
-# Wingard, C. 2017-02-15
+# Wingard, C. 2017-04-07
 
 # Parse the command line inputs
 if [ $# -ne 3 ]; then
@@ -23,18 +23,18 @@ LAT="46.85165"
 LNG="-124.98229"
 
 # Buoy
-$PROCESS/process_gps.sh $PLATFORM $DEPLOY $LAT $LNG "buoy/gps" $FNAME.gps.json
-for mopak in $PROC/$PLATFORM/$DEPLOY/buoy/mopak/$FNAME*.mopak.json; do
+#$PROCESS/process_gps.sh $PLATFORM $DEPLOY $LAT $LNG "buoy/gps" $FNAME.gps.json
+for mopak in $PROC/$PLATFORM/$DEPLOY/buoy/3dmgx3/$FNAME*.3dmgx3.json; do
     if [ -e $mopak ]; then
         SIZE=`du -k "$mopak" | cut -f1`
         if [ $SIZE > 0 ]; then
-            $PROCESS/process_mopak.sh $PLATFORM $DEPLOY $LAT $LNG "buoy/mopak" $mopak
+            $PROCESS/process_mopak.sh $PLATFORM $DEPLOY $LAT $LNG "buoy/3dmgx3" $mopak
         fi
     fi
 done
-# $PROCESS/process_superv_cpm.sh $PLATFORM $DEPLOY $LAT $LNG "buoy/superv/stc1" $FNAME.superv.json
+$PROCESS/process_superv_stc.sh $PLATFORM $DEPLOY $LAT $LNG "buoy/superv" $FNAME.syslog.json
 
-# IMM
+# IMM/MMP
 for mmp in $PROC/$PLATFORM/$DEPLOY/imm/mmp/P*.json; do
     SIZE=`du -k "$mmp" | cut -f1`
     if [ $SIZE > 0 ]; then
@@ -42,3 +42,5 @@ for mmp in $PROC/$PLATFORM/$DEPLOY/imm/mmp/P*.json; do
     fi
 done
 
+# IMM/ADCPS
+# <Add adcps processing here>
---
+++ utilities/processors/process_superv_stc.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+#
+# Read the parsed DCL SUPERV data files from the Endurance Surface Moorings and
+# create processed datasets available in NetCDF formatted files for further
+# processing and review.
+#
+# C. Wingard 2017-01-24
+
+# Parse the command line inputs
+if [ $# -ne 6 ]; then
+    echo "$0: required inputs are the platform and deployment names, the latitude and longitude, the DCL SUPERV"
+    echo " directory name, and the name of the file to process."
+    echo ""
+    echo "     example: $0 ce09ospm D00006 46.85165 -124.98229 buoy/superv 20161012.syslog.json"
+    exit 1
+fi
+PLATFORM=${1,,}
+DEPLOY=${2^^}
+LAT=$3; LNG=$4
+SUPERV=${5,,}
+FILE=`/bin/basename $6`
+
+# Set the default directory paths and input/output sources
+PYTHON="/home/ooiuser/bin/conda/bin/python3"
+
+DATA="/home/ooiuser/data"
+IN="$DATA/proc/$PLATFORM/$DEPLOY/$SUPERV/$FILE"
+OUT="$DATA/erddap/$PLATFORM/$DEPLOY/$SUPERV/${FILE%.json}.nc"
+if [ ! -d `/usr/bin/dirname $OUT` ]; then
+    mkdir -p `/usr/bin/dirname $OUT`
+fi
+
+# Process the file
+if [ -e $IN ]; then
+    cd /home/ooiuser/code/cgsn-processing
+    $PYTHON -m cgsn_processing.process.proc_superv_stc -p $PLATFORM -d $DEPLOY -lt $LAT -lg $LNG -i $IN -o $OUT
+fi
