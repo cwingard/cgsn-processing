48c8408c5f44f0764625b83bc86af6cd947be46a
Christopher Wingard <cwingard@coas.oregonstate.edu>
2019-06-14 14:33:19 -0700

    Initial updates to the CTDBP and DOSTA modules to incorporate xarray, add the IMM ctd type and processing of L0 data products to L1 and L2.

--- cgsn_processing/process/configs/attr_ctdbp.py
+++ cgsn_processing/process/configs/attr_ctdbp.py
@@ -6,49 +6,191 @@
 @author Christopher Wingard
 @brief Attributes for the CTDBP variables
 """
-CTDBP = {
+import numpy as np
+
+GLOBAL = {
+    # global attributes
+    'global': {
+        'title': 'CTD',
+        'summary': ('CTD Data'),
+        'project': 'Ocean Observatories Initiative',
+        'institution': 'Coastal and Global Scale Nodes (CGSN)',
+        'acknowledgement': 'National Science Foundation',
+        'references': 'http://oceanobservatories.org',
+        'creator_name': 'Ocean Observatories Initiative',
+        'creator_email': 'helpdesk@oceanobservatories.org',
+        'creator_url': 'http://oceanobservatories.org',
+        'featureType': 'timeSeries',
+        'cdm_data_type': 'Station',
+        'Conventions': 'CF-1.6'
+    },
     'deploy_id': {
         'long_name': 'Deployment ID',
-        'standard_name': 'deployment_id',
-        'units': '1',
-        'coordinates': 'time z longitude latitude',
-        'grid_mapping': 'crs',
-        'platform': 'platform',
-        'ancillary_variables': 'platform',
-        'coverage_content_type': 'physicalMeasurement'
+        'comment': ('Mooring deployment ID. Useful for differentiating data by deployment, ' +
+                    'allowing for overlapping deployments in the data sets.')
     },
-    'dcl_date_time_string': {
-        'long_name': 'DCL Date and Time Stamp',
-        'standard_name': 'dcl_date_time_string',
-        'units': '1',
-        'coordinates': 'time z longitude latitude',
-        'grid_mapping': 'crs',
-        'platform': 'platform',
-        'ancillary_variables': 'platform',
-        'coverage_content_type': 'physicalMeasurement'
+    'station': {
+        'cf_role': 'timeseries_id',
+        'long_name': 'Station Identifier'
     },
-    'ctd_date_time_string': {
-        'long_name': 'CTD Date and Time Stamp',
-        'standard_name': 'ctd_date_time_string',
-        'units': '1',
-        'coordinates': 'time z longitude latitude',
-        'grid_mapping': 'crs',
-        'platform': 'platform',
-        'ancillary_variables': 'platform',
-        'coverage_content_type': 'physicalMeasurement'
+    'time': {
+        'standard_name': 'time',
+        'units': 'seconds since 1970-01-01 00:00:00 0:00',
+        'axis': 'T',
+        'calendar': 'gregorian',
+        'comment': ('Derived from either the DCL data logger GPS referenced clock, or the internal instrument clock ' +
+                    'if this is an IMM hosted instrument. For instruments attached to a DCL, the instrument''s ' +
+                    'internal clock is cross-compared to the GPS clock to determine the internal clock''s time ' +
+                    'offset and drift.')
     },
-    'temperature': {
-        'units': 'degrees_Celsius'
+    'lon': {
+        'long_name': 'Longitude',
+        'standard_name': 'longitude',
+        'units': 'degrees_east',
+        'axis': 'X',
+        'comment': 'Mooring deployment location, surveyed after deployment to determine center of watch circle.'
+    },
+    'lat': {
+        'long_name': 'Latitude',
+        'standard_name': 'latitude',
+        'units': 'degrees_north',
+        'axis': 'Y',
+        'comment': 'Mooring deployment location, surveyed after deployment to determine center of watch circle.'
+    },
+    'z': {
+        'long_name': 'Depth',
+        'standard_name': 'depth',
+        'units': 'm',
+        'comment': 'Instrument deployment depth',
+        'positive': 'down',
+        'axis': 'Z'
+    }
+}
+
+CTDBP = {
+    # attributes for all instances of the CTDBP, regardless of system used to log data or instrument(s) attached.
+    # --> reported values
+    'sensor_time': {
+        'long_name': 'CTD Date and Time',
+        'standard_name': 'time',
+        'comment': ('Internal CTD clock date and time stamp, recorded when the instrument begins the measurement. It ' +
+                    'is expected that this value will drift from the true time by some degree over the course of ' +
+                    'a deployment. Cross-comparisons to other systems will be required to account for the offset ' +
+                    'and drift.'),
+        'units': 'seconds since 1970-01-01 00:00:00 0:00',
+        'calendar': 'gregorian'
     },
     'conductivity': {
-        'units': 'S/m'
+        'long_name': 'Seawater Conductivity',
+        'standard_name': 'sea_water_electrical_conductivity',
+        'units': 'mS cm-1',
+        'comment': ('Seawater conductivity refers to the ability of seawater to conduct electricity. The presence of ' +
+                    'ions, such as salt, increases the electrical conducting ability of seawater. As such, ' +
+                    'conductivity can be used as a proxy for determining the quantity of salt in a sample of ' +
+                    'seawater.'),
+        'data_product_identifier': 'CONDWAT_L1',
+        '_FillValue': np.nan
+    },
+    'temperature': {
+        'long_name': 'Seawater Temperature',
+        'standard_name': 'sea_water_temperature',
+        'units': 'degree_Celsius',
+        'comment': 'Seawater temperature at the sensor.',
+        'data_product_identifier': 'TEMPWAT_L1',
+        '_FillValue': np.nan
     },
     'pressure': {
-        'units': 'dbar'
+        'long_name': 'Seawater Pressure',
+        'standard_name': 'sea_water_pressure_due_to_sea_water',
+        'units': 'dbar',
+        'comment': (
+                    'Seawater Pressure refers to the pressure exerted on a sensor in situ by the weight of the column ' +
+                    'of seawater above it. It is calculated by subtracting one standard atmosphere from the absolute ' +
+                    'pressure at the sensor to remove the weight of the atmosphere on top of the water column. The ' +
+                    'pressure at a sensor in situ provides a metric of the depth of that sensor.'),
+        'data_product_identifier': 'PRESWAT_L1',
+        '_FillValue': np.nan
     },
+    # --> derived values
+    'salinity': {
+        'long_name': 'Practical Salinity',
+        'standard_name': 'sea_water_practical_salinity',
+        'units': '1',
+        'comment': ('Salinity is generally defined as the concentration of dissolved salt in a parcel of seawater. ' +
+                    'Practical Salinity is a more specific unitless quantity calculated from the conductivity of ' +
+                    'seawater and adjusted for temperature and pressure. It is approximately equivalent to Absolute ' +
+                    'Salinity (the mass fraction of dissolved salt in seawater) but they are not interchangeable.'),
+        'data_product_identifier': 'PRACSAL_L2',
+        'ancillary_variables': 'conductivity, temperature, pressure',
+        '_FillValue': np.nan
+    },
+    'density': {
+        'long_name': 'In-Situ Seawater Density',
+        'standard_name': 'sea_water_density',
+        'units': 'kg m-3',
+        'comment': ('Seawater Density is defined as mass per unit volume and is calculated from the conductivity, ' +
+                    'temperature and depth of a seawater sample using the TEOS-10 equation.'),
+        'data_product_identifier': 'DENSITY_L2',
+        'ancillary_variables': 'lon, lat, pressure, temperature, salinity',
+        '_FillValue': np.nan
+    },
+
+    # attributes associated with the status message output from a CTDBP connected to an inductive modem (IMM)
+    'status_time': {
+        'long_name': 'Status Update Time',
+        '_FillValue': np.nan,
+        'units': 'seconds since 1970-01-01 00:00:00 0:00',
+        'calendar': 'gregorian',
+        'comment': 'Date and time the CTD status was queried.'
+    },
+    'serial_number': {
+        'long_name': 'Serial Number',
+        '_FillValue': -9999999,
+        # 'units': ''    # deliberately left blank, no units for this value
+    },
+    'main_battery_voltage': {
+        'long_name': 'Main Battery Voltage',
+        'units': 'V',
+        '_FillValue': np.nan
+    },
+    'lithium_battery_voltage': {
+        'long_name': 'Lithium Battery Voltage',
+        'units': 'V',
+        '_FillValue': np.nan
+    },
+    'samples_recorded': {
+        'long_name': 'Number of Samples Recorded',
+        'comment': 'Number of samples recorded during the deployment',
+        'units': 'counts',
+        '_FillValue': -9999999,
+    },
+    'sample_slots_free': {
+        'long_name': 'Number of Free Sample Slots Remaining',
+        'comment': 'Number of free samples available for recording, representing the memory available in the unit',
+        'units': 'counts',
+        '_FillValue': -9999999,
+    },
+    'main_current': {},
+    'pump_current': {},
+    'oxy_current': {},
+    'eco_current': {},
+
+    # attributes associated with a CTDBP hosted by a DCL
+    'time_offset': {
+        'long_name': 'Internal Clock Offset',
+        'comment': ('Difference between the internal CTD clock and the external GPS-based data logger clock. Can ' +
+                    'be used to determine instrument clock offset and drift over the course of a deployment'),
+        'units': 'seconds',
+        'ancillary_variables': 'ctd_time, time'
+    },
+
+    # Values recorded by the different CTDBP configurations
+    # --> with an Aanderaa Optode (DOSTA)
     'oxygen_concentration': {
         'units': 'umol/L'
     },
+
+    # --> with WET Labs ECO Triplet (FLORT)
     'raw_backscatter': {
         'units': 'counts'
     },
@@ -58,18 +200,10 @@ CTDBP = {
     'raw_cdom': {
         'units': 'counts'
     },
-    'psu': {
-        'long_name': 'Practical Salinity',
-        'standard_name': 'sea_water_salinity',
-        'units': '1',
-        'valid_min': '0',
-        'valid_max': '40'
-    },
-    'rho': {
-        'long_name': 'In-Situ Seawater Density',
-        'standard_name': 'sea_water_density',
-        'units': 'kg m-3',
-        'valid_min': '0',
-        'valid_max': '35'
-    }
+
+    # --> with an Aanderaa Optode (DOSTA) and a WET Labs FLNTU (FLORD) (IMM Hosted)
+    'raw_oxy_calphase': {},
+    'raw_oxy_temp': {},
+    'raw_chlorophyll': {},
+    'raw_backscatter': {}
 }
--- cgsn_processing/process/proc_ctdbp.py
+++ cgsn_processing/process/proc_ctdbp.py
@@ -8,95 +8,196 @@
 """
 import numpy as np
 import os
-import re
+import xarray as xr
 
 from gsw import SP_from_C, SA_from_SP, CT_from_t, rho
-from pyaxiom.netcdf.sensors import TimeSeries
 
-from cgsn_processing.process.common import inputs, json2df
-from cgsn_processing.process.configs.attr_ctdbp import CTDBP
+from cgsn_processing.process.common import ENCODING, inputs, dict_update, epoch_time, join_df, \
+    json2df, json2obj, json_obj2df, update_dataset
+from cgsn_processing.process.finding_calibrations import find_calibration
+from cgsn_processing.process.configs.attr_ctdbp import GLOBAL, CTDBP
+
+from cgsn_processing.process.proc_dosta import Calibrations as DOSTA_Calibrations
+from cgsn_processing.process.proc_flort import Calibrations as FLORT_Calibrations
+
+from pyseas.data.do2_functions import do2_phase_volt_to_degree, do2_therm_volt_to_degc, \
+    do2_phase_to_doxy, do2_salinity_correction
+from pyseas.data.flo_functions import flo_scale_and_offset, flo_bback_total
 
 
 def main(argv=None):
     # load the input arguments
     args = inputs(argv)
     infile = os.path.abspath(args.infile)
-    outpath, outfile = os.path.split(args.outfile)
+    outfile = os.path.abspath(args.outfile)
     platform = args.platform
     deployment = args.deployment
     lat = args.latitude
     lon = args.longitude
     depth = args.depth
+    ctd_type = args.switch
 
-    # load the json data file and return a panda dataframe
-    df = json2df(infile)
-    if df.empty:
-        # there was no data in this file, ending early
-        return None
+    if ctd_type in ['solo', 'dosta', 'flort']:
+        # load the json data file as a panda data frame for further processing
+        ctd = json2df(infile)
+        if not ctd:
+            # json data file was empty, exiting
+            return None
 
-    df['depth'] = depth
-    df['deploy_id'] = deployment
+        ctd['sensor_time'] = epoch_time(ctd['ctd_date_time_string'].values[0])
+        ctd.drop(columns='ctd_date_time_string', inplace=True)
+
+        # create an xarray data set from the data frame
+        ctd = xr.Dataset.from_dataframe(ctd)
+        
+        if ctd_type == 'flort':
+            # grab the calibration coefficients for the FLORT
+            flort_coeff = os.path.join(os.path.dirname(infile), 'ctdbp-flort.cal_coeffs.pkl')
+            dev = FLORT_Calibrations(flort_coeff)  # initialize calibration class
+            if os.path.isfile(flort_coeff):
+                # we always want to use this file if it exists
+                dev.load_coeffs()
+            else:
+                # load from the CI hosted CSV files
+                csv_url = find_calibration('FLORT', args.flort_serial, (ctd.time.values.astype('int64') * 10 ** -9)[0])
+                if csv_url:
+                    dev.read_csv(csv_url)
+                    dev.save_coeffs()
+                else:
+                    print('A source for the FLORT calibration coefficients for {} could not be found, ' +
+                          'filling calibration coefficients with NaN'.format(infile))
+                    dev.coeffs['dark_cdom'] = np.nan
+                    dev.coeffs['scale_cdom'] = np.nan
+                    dev.coeffs['dark_chla'] = np.nan
+                    dev.coeffs['scale_chla'] = np.nan
+                    dev.coeffs['dark_beta'] = np.nan
+                    dev.coeffs['scale_beta'] = np.nan
+                    dev.coeffs['chi_factor'] = np.nan
+                    dev.coeffs['wavelength'] = np.nan
+                    dev.coeffs['scatter_angle'] = np.nan
+                    dev.save_coeffs()
+
+            ctd['estimated_chlorophyll'] = flo_scale_and_offset(ctd['raw_chlorophyll'], dev.coeffs['dark_chla'],
+                                                                dev.coeffs['scale_chla'])
+            ctd['fluorometric_cdom'] = flo_scale_and_offset(ctd['raw_cdom'], dev.coeffs['dark_cdom'],
+                                                            dev.coeffs['scale_cdom'])
+            ctd['beta_700'] = flo_scale_and_offset(ctd['raw_backscatter'], dev.coeffs['dark_beta'],
+                                                   dev.coeffs['scale_beta'])
+
+    elif ctd_type == 'imm':
+        # load the json data file as a json formatted object for further processing
+        data = json2obj(infile)
+        if not data:
+            # json data file was empty, exiting
+            return None
+
+        # create a data frame with the CTD status information
+        status = json_obj2df(data, 'status')
+        status['status_time'] = epoch_time(status['date_time_string'].values[0])
+        status.drop(columns='date_time_string', inplace=True)
+        status.rename(columns={'main_battery': 'main_battery_voltage',
+                               'lithium_battery': 'lithium_battery_voltage',
+                               'memory_free': 'sample_slots_free'},
+                      inplace=True)
+
+        # create a data frame with the CTD, DOSTA and FLORD data
+        ctd = json_obj2df(data, 'ctd')
+        ctd['sensor_time'] = epoch_time(status['date_time_string'].values[0])
+        ctd.drop(columns='date_time_string', inplace=True)
+        ctd.rename(columns={'raw_oxy_calphase': 'raw_oxygen_phase',
+                            'raw_oxy_temp': 'raw_oxygen_thermistor'},
+                   inplace=True)
+
+        # join the status and ctd data together into a single data frame, keeping track of data types and fill values
+        joined = join_df(ctd, status)
+
+        # create a final data set with the raw and derived CTD data and merged status data
+        ctd = xr.Dataset.from_dataframe(joined)
+
+        # grab the calibration coefficients for the two sensors: FLORD
+        flort_coeff = os.path.join(os.path.dirname(infile), 'ctdmo-flord.cal_coeffs.pkl')
+        dev = FLORT_Calibrations(flort_coeff)  # initialize calibration class
+        if os.path.isfile(flort_coeff):
+            # we always want to use this file if it exists
+            dev.load_coeffs()
+        else:
+            # load from the CI hosted CSV files
+            csv_url = find_calibration('FLORD', args.flort_serial, (ctd.time.values.astype('int64') * 10 ** -9)[0])
+            if csv_url:
+                dev.read_csv(csv_url)
+                dev.save_coeffs()
+            else:
+                print('A source for the FLORD calibration coefficients for {} could not be found, ' +
+                      'filling calibration coefficients with NaN'.format(infile))
+                dev.coeffs['dark_chla'] = np.nan
+                dev.coeffs['scale_chla'] = np.nan
+                dev.coeffs['dark_beta'] = np.nan
+                dev.coeffs['scale_beta'] = np.nan
+                dev.coeffs['chi_factor'] = np.nan
+                dev.coeffs['wavelength'] = np.nan
+                dev.coeffs['scatter_angle'] = np.nan
+                dev.save_coeffs()
+
+        # grab the calibration coefficients for the two sensors: DOSTA
+        dosta_coeff = os.path.join(os.path.dirname(infile), 'ctdmo-dosta.cal_coeffs.pkl')
+        dev = DOSTA_Calibrations(dosta_coeff)  # initialize calibration class
+        if os.path.isfile(dosta_coeff):
+            # we always want to use this file if it exists
+            dev.load_coeffs()
+        else:
+            # load from the CI hosted CSV files
+            csv_url = find_calibration('DOSTA', args.dosta_serial, (ctd.time.values.astype('int64') * 10 ** -9)[0])
+            if csv_url:
+                dev.read_csv(csv_url)
+                dev.save_coeffs()
+            else:
+                print('A source for the DOSTA calibration coefficients for {} could not be found, ' +
+                      'filling calibration coefficients with NaN'.format(infile))
+                dev.coeffs['two_point_coeffs'] = np.ones(2) * np.nan
+                dev.coeffs['svu_cal_coeffs'] = np.ones(5) * np.nan
+                dev.save_coeffs()
+
+        # compute the L0 to L1 conversions
+        ctd['oxygen_phase'] = do2_phase_volt_to_degree(ctd['raw_oxygen_phase'])
+        ctd['oxygen_thermistor'] = do2_therm_volt_to_degc(ctd['raw_oxygen_thermistor'])
+        ctd['dissolved_oxygen'] = do2_phase_to_doxy(ctd['oxygen_phase'], ctd['oxygen_thermistor'],
+                                                    dev.coeffs['svu_cal_coeffs'], dev.coeffs['two_point_coeffs'])
+        ctd['estimated_chlorophyll'] = flo_scale_and_offset(ctd['raw_chlorophyll'], dev.coeffs['dark_chla'],
+                                                            dev.coeffs['scale_chla'])
+        ctd['beta_700'] = flo_scale_and_offset(ctd['raw_backscatter'], dev.coeffs['dark_beta'],
+                                               dev.coeffs['scale_beta'])
+    else:
+        pass
+
+    # add the deployment id, used to subset data sets
+    ctd['deploy_id'] = deployment
 
     # calculate the practical salinity of the seawater from the temperature and conductivity measurements
-    df['psu'] = SP_from_C(df['conductivity'] * 10.0, df['temperature'], df['pressure'])
+    ctd['salinity'] = SP_from_C(ctd['conductivity'] * 10.0, ctd['temperature'], ctd['pressure'])
+
     # calculate the in-situ density of the seawater from the absolute salinity and conservative temperature
-    sa = SA_from_SP(df['psu'], df['pressure'], lon, lat)                 # absolute salinity
-    ct = CT_from_t(sa, df['temperature'], df['pressure'])    # conservative temperature
-    df['rho'] = rho(sa, ct, df['pressure'])                              # density
+    sa = SA_from_SP(ctd['psu'], ctd['pressure'], lon, lat)      # absolute salinity
+    ct = CT_from_t(sa, ctd['temperature'], ctd['pressure'])     # conservative temperature
+    ctd['density'] = rho(sa, ct, ctd['pressure'])               # density
 
-    # TODO: If CTD with attached FLORT, add code to convert raw counts to scientific units
+    if ctd_type in ['dosta', 'imm']:
+        # apply temperature, salinity and pressure corrections to dissolved oxygen measurement
+        ctd['corrected_dissolved_oxygen'] = do2_salinity_correction(ctd['dissolved_oxygen'], ctd['pressure'],
+                                                                    ctd['temperature'], ctd['salinity'], lat, lon)
 
-    # Setup the global attributes for the NetCDF file and create a timeseries object
-    global_attributes = {
-        'title': 'CTD Data Records',
-        'summary': (
-            'Records the CTD data, plus any attached sensors for the Mooring NSIF and MFN platforms'
-        ),
-        'project': 'Ocean Observatories Initiative',
-        'institution': 'Coastal and Global Scales Nodes, (CGSN)',
-        'acknowledgement': 'National Science Foundation',
-        'references': 'http://oceanobservatories.org',
-        'creator_name': 'Christopher Wingard',
-        'creator_email': 'cwingard@coas.oregonstate.edu',
-        'creator_url': 'http://oceanobservatories.org',
-        'comment': 'Mooring ID: {}-{}'.format(platform.upper(), re.sub('\D', '', deployment))
-    }
-    ts = TimeSeries(
-        output_directory=outpath,
-        latitude=lat,
-        longitude=lon,
-        station_name=platform,
-        global_attributes=global_attributes,
-        times=df.time.values.astype(np.int64) * 10**-9,
-        verticals=df.depth.values,
-        output_filename=outfile,
-        vertical_positive='down')
+    if ctd_type in ['flort', 'imm']:
+        # calculate the total optical backscatter, corrected for in-situ salinity, from the volume scattering
+        # coefficient (Beta) at 700 nm.
+        ctd['bback'] = flo_bback_total(ctd['beta_700'], ctd['temperature'], ctd['salinity'],
+                                       dev.coeffs['scatter_angle'], dev.coeffs['wavelength'], dev.coeffs['chi_factor'])
 
-    # add the data from the data frame and set the attributes
-    nc = ts._nc     # create a netCDF4 object from the TimeSeries object
+    # assign/create needed dimensions, geo coordinates and update the metadata attributes for the data set
+    attrs = dict_update(GLOBAL, CTDBP)
+    ctd = update_dataset(ctd, platform, deployment, lat, lon, [depth, depth, depth], attrs)
 
-    for c in df.columns:
-        # skip the coordinate variables, if present, already added above via TimeSeries
-        if c in ['time', 'latitude', 'longitude', 'depth']:
-            # print("Skipping axis '{}' (already in file)".format(c))
-            continue
+    # save the data
+    ctd.to_netcdf(outfile, mode='w', format='NETCDF4', engine='netcdf4', encoding=ENCODING)
 
-        # create the netCDF.Variable object for the date/time string
-        if c in ['dcl_date_time_string', 'ctd_date_time_string']:
-            d = nc.createVariable(c, 'S23', ('time',))
-            d.setncatts(CTDBP[c])
-            d[:] = df[c].values
-        elif c == 'deploy_id':
-            d = nc.createVariable(c, 'S6', ('time',))
-            d.setncatts(CTDBP[c])
-            d[:] = df[c].values
-        else:
-            # use the TimeSeries object to add the variables
-            ts.add_variable(c, df[c].values, fillvalue=-999999999, attributes=CTDBP[c])
-
-    # synchronize the data with the netCDF file and close it
-    nc.sync()
-    nc.close()
 
 if __name__ == '__main__':
     main()
