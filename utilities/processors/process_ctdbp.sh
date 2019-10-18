df99c6b47b228b1768729f2337d4e07acc8618fe
Christopher Wingard <wingardc@oregonstate.edu>
2019-10-18 14:42:28 -0700

    Update example string for CTDBP processing script

--- utilities/processors/process_ctdbp.sh
+++ utilities/processors/process_ctdbp.sh
@@ -9,10 +9,12 @@
 # Parse the command line inputs
 if [ $# -ne 9 ]; then
     echo "$0: required inputs are the platform and deployment names, the latitude and longitude, the CTDBP"
-    echo " directory name, the deployment depth, the serial number of the FLORT (if applicable), a switch to"
+    echo " directory name, the deployment depth, the serial number of the DOSTA or FLORT (if applicable), a switch to"
     echo " indicate the configuration of the CTDBP (solo, dosta or flort) and the name of the file to process."
     echo ""
-    echo "     example: $0 ce07shsm D00004 44.63929 -124.30404 mfn/ctdbp 87 0 dosta 20161012.ctdbp2.json"
+    echo "     example (SOLO): $0 ce07shsm D00004 44.63929 -124.30404 nsif/ctdbp 7 NaN solo 20161012.ctdbp1.json"
+    echo "     example (DOSTA): $0 ce07shsm D00004 44.63929 -124.30404 mfn/ctdbp 87 477 dosta 20161012.ctdbp2.json"
+    echo "     example (FLORT): $0 ce06issm D00005 44.63929 -124.30404 buoy/ctdbp 1 1154 flort 20161012.ctdbp3.json"
     exit 1
 fi
 PLATFORM=${1,,}
