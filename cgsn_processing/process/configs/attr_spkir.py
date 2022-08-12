e93645c73717b7b9e66a920d8b36cff3c26443de
Christopher Wingard <chris.wingard@oregonstate.edu>
2022-08-12 14:55:18 -0700

    Updating the SPKIR processor and attributes

--- cgsn_processing/process/configs/attr_spkir.py
+++ cgsn_processing/process/configs/attr_spkir.py
@@ -6,70 +6,195 @@
 @author Joe Futrelle
 @brief Attributes for the SPKIR variables
 """
+import numpy as np
 
 SPKIR = {
+    # global attributes
     'global': {
-        'title': 'Downwelling Spectral Irradiance at 7 meters',
-        'summary': 'Downwelling spectral irradiance measured over 7 wavelengths at 7 meter depth',
-        'project': 'Ocean Observatories Initiative',
-        'institution': 'Coastal and Global Scale Nodes, (CGSN)',
-        'acknowledgement': 'National Science Foundation',
-        'references': 'http://oceanobservatories.org',
-        'creator_name': 'Christopher Wingard',
-        'creator_email': 'cwingard@coas.oregonstate.edu',
-        'creator_url': 'http://oceanobservatories.org',
+        'title': 'Downwelling Spectral Irradiance',
+        'summary': ('Downwelling spectral irradiance measured at 7 discrete wavelengths with the Sea-Bird Electronics '
+                    '(formerly Satlantic) OCR-507 Multispectral Radiometer.'),
     },
-    'deploy_id': {
-        'long_name': 'Deployment ID',
-        'standard_name': 'deployment_id',
-        'units': '1',
-    },
-    'dcl_date_time_string': {
-        'long_name': 'DCL Date and Time Stamp',
-        'standard_name': 'dcl_date_time_string',
-        'units': '1',
-    },
-    'z': {
-        'long_name': 'Sensor Depth',
-        'standard_name': 'depth',
-        'units': 'm',
-        'positive': 'down',
-        'axis': 'Z',
-        'valid_min': '-10000',
-        'valid_max': '1000',
-    },
+    # dataset attributes --> reported values
     'serial_number': {
-        'units': '1'
+        'long_name': 'Serial Number',
+        # 'units': '',    # deliberately left blank, no units for this value,
+        'comment': ('A string denoting the serial number of the instrument. This field combined with the text '
+                    'SATDI7 uniquely identifies the instrument. This combination is known as the frame header or '
+                    'synchronization string. This is normally a four-character field.'),
     },
     'timer': {
-        'units': 's'
+        'long_name': 'Timer',
+        'units': 's',
+        'comment': ('Indicates the number of seconds that have passed since the end of the initialization sequence. '
+                    'This field is precise to two decimal places.')
     },
     'sample_delay': {
-        'units': 'ms'
+        'long_name': 'Sample Delay',
+        'units': 'ms',
+        'comment': ('The number of milliseconds to offset the Timer value to give an accurate indication of when '
+                    'the frame''s sensors were sampled.')
     },
-    'raw_channels': {
-        'units': 'count'
+    'raw_irradiance_412': {
+        'long_name': 'Raw Downwelling Irradiance 412 nm',
+        'comment': ('Raw downwelling spectral irradiance at 412 nm measured by the Sea-Bird Electronics (formerly '
+                   'Satlantic) OCR-507 sensor.'),
+        'units': 'count',
+        'data_product_identifier': 'SPECTIR_L0-412',
+    },
+    'raw_irradiance_444': {
+        'long_name': 'Raw Downwelling Irradiance 444 nm',
+        'comment': ('Raw downwelling spectral irradiance at 444 nm measured by the Sea-Bird Electronics (formerly '
+                   'Satlantic) OCR-507 sensor.'),
+        'units': 'count',
+        'data_product_identifier': 'SPECTIR_L0-444',
+    },
+    'raw_irradiance_490': {
+        'long_name': 'Raw Downwelling Irradiance 490 nm',
+        'comment': ('Raw downwelling spectral irradiance at 490 nm measured by the Sea-Bird Electronics (formerly '
+                   'Satlantic) OCR-507 sensor.'),
+        'units': 'count',
+        'data_product_identifier': 'SPECTIR_L0-490',
+    },
+    'raw_irradiance_510': {
+        'long_name': 'Raw Downwelling Irradiance 510 nm',
+        'comment': ('Raw downwelling spectral irradiance at 510 nm measured by the Sea-Bird Electronics (formerly '
+                   'Satlantic) OCR-507 sensor.'),
+        'units': 'count',
+        'data_product_identifier': 'SPECTIR_L0-510',
+    },
+    'raw_irradiance_555': {
+        'long_name': 'Raw Downwelling Irradiance 555 nm',
+        'comment': ('Raw downwelling spectral irradiance at 555 nm measured by the Sea-Bird Electronics (formerly '
+                   'Satlantic) OCR-507 sensor.'),
+        'units': 'count',
+        'data_product_identifier': 'SPECTIR_L0-555',
+    },
+    'raw_irradiance_620': {
+        'long_name': 'Raw Downwelling Irradiance 620 nm',
+        'comment': ('Raw downwelling spectral irradiance at 620 nm measured by the Sea-Bird Electronics (formerly '
+                   'Satlantic) OCR-507 sensor.'),
+        'units': 'count',
+        'data_product_identifier': 'SPECTIR_L0-620',
+    },
+    'raw_irradiance_683': {
+        'long_name': 'Raw Downwelling Irradiance 683 nm',
+        'comment': ('Raw downwelling spectral irradiance at 683 nm measured by the Sea-Bird Electronics (formerly '
+                   'Satlantic) OCR-507 sensor.'),
+        'units': 'count',
+        'data_product_identifier': 'SPECTIR_L0-683',
     },
     'input_voltage': {
-        'units': 'V'
+        'long_name': 'Input Voltage',
+        'units': 'V',
+        'comment': 'Voltage supplied by the DCL to power the instrument.'
     },
     'analog_rail_voltage': {
-        'units': 'V'
-    },
-    'frame_counter': {
-        'units': '1'
+        'long_name': 'Analog Rail Voltage',
+        'units': 'V',
+        'comment': 'Indicates the analog rail voltage for the operational components of the instrument.',
     },
     'internal_temperature': {
-        'units': 'degrees_Celsius'
+        'long_name': 'Internal Temperature',
+        'units': 'degrees_Celsius',
+        'comment': 'Internal temperature of the instrument.',
     },
-    'irradiance': {
-        'long_name': 'Downwelling Spectral Irradiance',
-        'standard_name': 'downwelling_spectral_spherical_irradiance_in_sea_water',
-        'units': 'uW cm-2 nm-1'
+    'frame_counter': {
+        'long_name': 'Frame Counter',
+        'units': 'count',
+        'comment': ('A data integrity sensor that maintains a count of each frame transmitted. The count increments '
+                    'by one for each frame transmitted from 0 to 255, at which point it rolls back to zero again.'),
     },
-    'wavelengths': {
-        'long_name': 'Radiation Wavelength',
-        'standard_name': 'radiation_wavelength',
-        'units': 'nm'
-    }
+    # dataset attributes --> derived values
+    'downwelling_irradiance_412': {
+        'long_name': 'Downwelling Spectral Irradiance at 412 nm',
+        'standard_name': 'downwelling_photon_spherical_irradiance_per_unit_wavelength_in_sea_water',
+        'units': 'uW cm-2 nm-1',
+        'comment': ('Downwelling spectral irradiance measured at 412 nm by the Sea-Bird Electronics OCR-507 '
+                    'Multispectral Radiometer. Spectral irradiance is a critical measurement for defining important '
+                    'ocean processes, such as the radiant heating rate, and sets the energy available to drive a '
+                    'range of biological and chemical processes in the ocean.'),
+        'data_product_identifier': 'SPECTIR_L1-412',
+        'radiation_wavelength': 412,
+        '_FillValue': np.nan,
+        'ancillary_variables': 'raw_irradiance_412'
+    },
+    'downwelling_irradiance_444': {
+        'long_name': 'Downwelling Spectral Irradiance at 444 nm',
+        'standard_name': 'downwelling_photon_spherical_irradiance_per_unit_wavelength_in_sea_water',
+        'units': 'uW cm-2 nm-1',
+        'comment': ('Downwelling spectral irradiance measured at 444 nm by the Sea-Bird Electronics OCR-507 '
+                    'Multispectral Radiometer. Spectral irradiance is a critical measurement for defining important '
+                    'ocean processes, such as the radiant heating rate, and sets the energy available to drive a '
+                    'range of biological and chemical processes in the ocean.'),
+        'data_product_identifier': 'SPECTIR_L1-444',
+        'radiation_wavelength': 444,
+        '_FillValue': np.nan,
+        'ancillary_variables': 'raw_irradiance_444'
+    },
+    'downwelling_irradiance_490': {
+        'long_name': 'Downwelling Spectral Irradiance at 490 nm',
+        'standard_name': 'downwelling_photon_spherical_irradiance_per_unit_wavelength_in_sea_water',
+        'units': 'uW cm-2 nm-1',
+        'comment': ('Downwelling spectral irradiance measured at 490 nm by the Sea-Bird Electronics OCR-507 '
+                    'Multispectral Radiometer. Spectral irradiance is a critical measurement for defining important '
+                    'ocean processes, such as the radiant heating rate, and sets the energy available to drive a '
+                    'range of biological and chemical processes in the ocean.'),
+        'data_product_identifier': 'SPECTIR_L1-490',
+        'radiation_wavelength': 490,
+        '_FillValue': np.nan,
+        'ancillary_variables': 'raw_irradiance_490'
+    },
+    'downwelling_irradiance_510': {
+        'long_name': 'Downwelling Spectral Irradiance at 510 nm',
+        'standard_name': 'downwelling_photon_spherical_irradiance_per_unit_wavelength_in_sea_water',
+        'units': 'uW cm-2 nm-1',
+        'comment': ('Downwelling spectral irradiance measured at 510 nm by the Sea-Bird Electronics OCR-507 '
+                    'Multispectral Radiometer. Spectral irradiance is a critical measurement for defining important '
+                    'ocean processes, such as the radiant heating rate, and sets the energy available to drive a '
+                    'range of biological and chemical processes in the ocean.'),
+        'data_product_identifier': 'SPECTIR_L1-510',
+        'radiation_wavelength': 510,
+        '_FillValue': np.nan,
+        'ancillary_variables': 'raw_irradiance_510'
+    },
+    'downwelling_irradiance_555': {
+        'long_name': 'Downwelling Spectral Irradiance at 555 nm',
+        'standard_name': 'downwelling_photon_spherical_irradiance_per_unit_wavelength_in_sea_water',
+        'units': 'uW cm-2 nm-1',
+        'comment': ('Downwelling spectral irradiance measured at 555 nm by the Sea-Bird Electronics OCR-507 '
+                    'Multispectral Radiometer. Spectral irradiance is a critical measurement for defining important '
+                    'ocean processes, such as the radiant heating rate, and sets the energy available to drive a '
+                    'range of biological and chemical processes in the ocean.'),
+        'data_product_identifier': 'SPECTIR_L1-555',
+        'radiation_wavelength': 555,
+        '_FillValue': np.nan,
+        'ancillary_variables': 'raw_irradiance_555'
+    },
+    'downwelling_irradiance_620': {
+        'long_name': 'Downwelling Spectral Irradiance at 620 nm',
+        'standard_name': 'downwelling_photon_spherical_irradiance_per_unit_wavelength_in_sea_water',
+        'units': 'uW cm-2 nm-1',
+        'comment': ('Downwelling spectral irradiance measured at 620 nm by the Sea-Bird Electronics OCR-507 '
+                    'Multispectral Radiometer. Spectral irradiance is a critical measurement for defining important '
+                    'ocean processes, such as the radiant heating rate, and sets the energy available to drive a '
+                    'range of biological and chemical processes in the ocean.'),
+        'data_product_identifier': 'SPECTIR_L1-620',
+        'radiation_wavelength': 620,
+        '_FillValue': np.nan,
+        'ancillary_variables': 'raw_irradiance_620'
+    },
+    'downwelling_irradiance_683': {
+        'long_name': 'Downwelling Spectral Irradiance at 683 nm',
+        'standard_name': 'downwelling_photon_spherical_irradiance_per_unit_wavelength_in_sea_water',
+        'units': 'uW cm-2 nm-1',
+        'comment': ('Downwelling spectral irradiance measured at 683 nm by the Sea-Bird Electronics OCR-507 '
+                    'Multispectral Radiometer. Spectral irradiance is a critical measurement for defining important '
+                    'ocean processes, such as the radiant heating rate, and sets the energy available to drive a '
+                    'range of biological and chemical processes in the ocean.'),
+        'data_product_identifier': 'SPECTIR_L1-683',
+        'radiation_wavelength': 683,
+        '_FillValue': np.nan,
+        'ancillary_variables': 'raw_irradiance_683'
+    },
 }
--- cgsn_processing/process/proc_spkir.py
+++ cgsn_processing/process/proc_spkir.py
@@ -3,31 +3,32 @@
 """
 @package cgsn_processing.process.proc_spkir
 @file cgsn_processing/process/proc_spkir.py
-@author Joe Futrelle
+@author Joe Futrelle, Chris Wingard
 @brief Creates a NetCDF dataset for SPKIR from JSON formatted source data
 """
 import json
 import numpy as np
 import os
 import pandas as pd
-import re
+import xarray as xr
 
-from netCDF4 import Dataset
-from pocean.utils import dict_update
-from pocean.dsg.timeseries.om import OrthogonalMultidimensionalTimeseries as OMTs
-
-from cgsn_processing.process.common import Coefficients, inputs, json2df, df2omtdf
+from cgsn_processing.process.common import Coefficients, inputs, json2df, update_dataset, \
+    ENCODING, dict_update
 from cgsn_processing.process.finding_calibrations import find_calibration
 from cgsn_processing.process.configs.attr_spkir import SPKIR
+from cgsn_processing.process.configs.attr_common import SHARED
+
 from pyseas.data.opt_functions import opt_ocr507_irradiance
 
 
 class Calibrations(Coefficients):
     def __init__(self, coeff_file, csv_url=None):
         """
-        Loads the SPKIR factory calibration coefficients for a unit. Values come from either a serialized object
-        created per instrument and deployment (calibration coefficients do not change in the middle of a deployment),
-        or from parsed CSV files maintained on GitHub by the OOI CI team.
+        Loads the SPKIR factory calibration coefficients for a unit. Values
+        come from either a serialized object created per instrument and
+        deployment (calibration coefficients do not change in the middle of a
+        deployment), or from parsed CSV files maintained on GitHub by the OOI
+        CI team.
         """
         # assign the inputs
         Coefficients.__init__(self, coeff_file)
@@ -35,8 +36,9 @@ class Calibrations(Coefficients):
 
     def read_csv(self, csv_url):
         """
-        Reads the values from a SPKIR calibration file already parsed and stored on Github as a CSV files. Note, 
-        the formatting of those files puts some constraints on this process. If someone has a cleaner method, 
+        Reads the values from a SPKIR calibration file already parsed and
+        stored on GitHub as a CSV files. Note, the formatting of those files
+        puts some constraints on this process. If someone has a cleaner method,
         I'm all in favor... 
         """
         # create the device file dictionary and assign values
@@ -57,6 +59,132 @@ class Calibrations(Coefficients):
         self.coeffs = coeffs
 
 
+def proc_spkir(infile, platform, deployment, lat, lon, depth, **kwargs):
+    """
+    Main SPKIR processing function. Loads the JSON formatted, parsed data and
+    applies appropriate calibration coefficients to convert the raw, parsed
+    data into engineering units. If calibration coefficients are available,
+    the dataset processing level attribute is set to "processed". Otherwise,
+    filled variables are returned and the dataset processing level attribute
+    is set to "parsed".
+
+    :param infile: JSON formatted parsed data file
+    :param platform: Name of the mooring the instrument is mounted on.
+    :param deployment: Name of the deployment for the input data file.
+    :param lat: Latitude of the mooring deployment.
+    :param lon: Longitude of the mooring deployment.
+    :param depth: Depth of the platform the instrument is mounted on.
+
+    :kwargs burst: Boolean flag to indicate whether to apply burst averaging
+            to the data. Default is to not apply burst averaging.
+
+    :return spkir: An xarray dataset with the processed spkir data
+    """
+    # process the variable length keyword arguments
+    burst = kwargs.get('burst')
+
+    # load the json data file as a dictionary object for further processing
+    data = json2df(infile)
+    if data.empty:
+        # json data file was empty, exiting
+        return None
+
+    # set up the instrument calibration data object
+    coeff_file = os.path.join(os.path.dirname(infile), 'spkir.cal_coeffs.json')
+    dev = Calibrations(coeff_file)  # initialize calibration class
+    proc_flag = False
+
+    # check for the source of the calibration coefficients and load accordingly
+    if os.path.isfile(coeff_file):
+        # we always want to use this file if it already exists
+        dev.load_coeffs()
+        proc_flag = True
+    else:
+        # load from the CI hosted CSV files
+        serial_number = str(data.serial_number[0])
+        sampling_time = data['time'][0].value / 10.0 ** 9
+        csv_url = find_calibration('SPKIR', serial_number, sampling_time)
+        if csv_url:
+            dev.read_csv(csv_url)
+            dev.save_coeffs()
+            proc_flag = True
+
+    # clean up the dataframe, getting rid of variables we don't need
+    data.drop(columns=['dcl_date_time_string'], inplace=True)
+
+    # create the time coordinate array and set up a base data frame
+    spkir_time = data['time']
+    df = pd.DataFrame()
+    df['time'] = pd.to_datetime(spkir_time, unit='s')
+    df.set_index('time', drop=True, inplace=True)
+
+    # pop the raw_channels array out of the dataframe (will put it back in later)
+    channels = np.array(np.vstack(data.pop('raw_channels')), dtype='uint32')
+
+    # set up and load the 1D parsed data into the data frame
+    for v in data.columns:
+        if v not in ['time']:
+            df[v] = np.atleast_1d(data[v])
+
+    # convert raw voltages and the instrument temperature to engineering units
+    df['input_voltage'].apply(lambda x: x * 0.03)
+    df['analog_rail_voltage'].apply(lambda x: x * 0.03)
+    df['internal_temperature'].apply(lambda x: -50 + x * 0.5)
+    
+    # add the raw downwelling spectral irradiance measurements to the dataframe per wavelength
+    df['raw_irradiance_412'] = channels[:, 0]
+    df['raw_irradiance_444'] = channels[:, 1]
+    df['raw_irradiance_490'] = channels[:, 2]
+    df['raw_irradiance_510'] = channels[:, 3]
+    df['raw_irradiance_555'] = channels[:, 4]
+    df['raw_irradiance_620'] = channels[:, 5]
+    df['raw_irradiance_683'] = channels[:, 6]
+
+    # Convert raw spectral irradiance values from count to uW cm-2 nm-1
+    if proc_flag:
+        ed = opt_ocr507_irradiance(channels, dev.coeffs['offset'], dev.coeffs['scale'], dev.coeffs['immersion_factor'])
+    else:
+        ed = channels.astype(float) * np.nan
+
+    # add the converted downwelling spectral irradiance measurements to the dataframe per wavelength
+    df['downwelling_irradiance_412'] = ed[:, 0]
+    df['downwelling_irradiance_444'] = ed[:, 1]
+    df['downwelling_irradiance_490'] = ed[:, 2]
+    df['downwelling_irradiance_510'] = ed[:, 3]
+    df['downwelling_irradiance_555'] = ed[:, 4]
+    df['downwelling_irradiance_620'] = ed[:, 5]
+    df['downwelling_irradiance_683'] = ed[:, 6]
+
+    # convert the now 1D dataframe into an xarray data set
+    spkir = xr.Dataset.from_dataframe(df)
+
+    # apply a median average to the burst (if desired)
+    if burst:
+        # resample to a 15-minute interval
+        spkir = spkir.resample(time='900s', base=3150, loffset='450s').median(dim='time', keep_attrs=True)
+
+        # resampling will fill in missing time steps with NaNs. Use the serial_number variable
+        # as a proxy variable to find cases where data is filled with a NaN, and delete those records.
+        spkir = spkir.where(~np.isnan(spkir.serial_number), drop=True)
+
+        # reset original integer values
+        int_arrays = ['frame_counter', 'sample_delay', 'serial_number']
+        for var in spkir.variables:
+            if var in int_arrays:
+                spkir[var] = spkir[var].astype(int)
+
+    # assign/create needed dimensions, geo coordinates and update the metadata attributes for the data set
+    spkir['deploy_id'] = xr.Variable(('time',), np.repeat(deployment, len(spkir.time)).astype(str))
+    attrs = dict_update(SPKIR, SHARED)
+    spkir = update_dataset(spkir, platform, deployment, lat, lon, [depth, depth, depth], attrs)
+    if proc_flag:
+        spkir.attrs['processing_level'] = 'processed'
+    else:
+        spkir.attrs['processing_level'] = 'parsed'
+
+    return spkir
+
+
 def main(argv=None):
     # load the input arguments
     args = inputs(argv)
@@ -67,73 +195,13 @@ def main(argv=None):
     lat = args.latitude
     lon = args.longitude
     depth = args.depth
+    burst = args.burst
 
-    # load the json data file and return a panda dataframe, adding a default depth and the deployment ID
-    df = json2df(infile)
-    if df.empty:
-        # there was no data in this file, ending early
-        return None
+    # process the SPKIR data and save the results to disk
+    spkir = proc_spkir(infile, platform, deployment, lat, lon, depth, burst=burst)
+    if spkir:
+        spkir.to_netcdf(outfile, mode='w', format='NETCDF4', engine='h5netcdf', encoding=ENCODING)
 
-    coeff_file = os.path.abspath(args.coeff_file)
-    dev = Calibrations(coeff_file)  # initialize calibration class
-
-    # check for the source of calibration coeffs and load accordingly
-    if os.path.isfile(coeff_file):
-        # we always want to use this file if it exists
-        dev.load_coeffs()
-    else:
-        # load from the CI hosted CSV files
-        csv_url = find_calibration('SPKIR', str(df.serial_number[0]), (df.time.values.astype('int64') * 10**-9)[0])
-        if csv_url:
-            dev.read_csv(csv_url)
-            dev.save_coeffs()
-        else:
-            print('A source for the SPKIR calibration coefficients for {} could not be found'.format(infile))
-            return None
-
-    # pop the raw_channels array out of the dataframe (will put it back in later)
-    channels = np.array(np.vstack(df.pop('raw_channels')), dtype='uint32')
-    # Convert spectral irradiance values from counts to uE/m^2/s
-    wavelengths = [412, 444, 490, 510, 555, 620, 683]
-    Ed = opt_ocr507_irradiance(channels, dev.coeffs['offset'], dev.coeffs['scale'], dev.coeffs['immersion_factor'])
-
-    # convert voltages and temperature to engineering units
-    df['input_voltage'].apply(lambda x: x * 0.03)
-    df['analog_rail_voltage'].apply(lambda x: x * 0.03)
-    df['internal_temperature'].apply(lambda x: -50 + x * 0.5)
-
-    # convert the dataframe to a format suitable for the pocean OMTs
-    df['deploy_id'] = deployment
-    df = df2omtdf(df, lat, lon, depth)
-
-    # add to the global attributes for the SPKIR
-    attrs = SPKIR
-    attrs['global'] = dict_update(attrs['global'], {
-        'comment': 'Mooring ID: {}-{}'.format(platform.upper(), re.sub('\D', '', deployment))
-    })
-
-    nc = OMTs.from_dataframe(df, outfile, attributes=attrs)
-    nc.close()
-
-    # re-open the netcdf file and add the raw channels, the downwelling irradiance and the wavelengths with the
-    # additional dimension of the measurement wavelengths.
-    nc = Dataset(outfile, 'a')
-    nc.createDimension('wavelengths', 7)
-
-    d = nc.createVariable('wavelengths', 'i', ('wavelengths',))
-    d.setncatts(attrs['wavelengths'])
-    d[:] = wavelengths
-
-    d = nc.createVariable('raw_channels', 'u4', ('station', 't', 'wavelengths',))
-    d.setncatts(attrs['raw_channels'])
-    d[:] = channels
-
-    d = nc.createVariable('irradiance', 'f', ('station', 't', 'wavelengths',))
-    d.setncatts(attrs['irradiance'])
-    d[:] = Ed
-
-    nc.sync()
-    nc.close()
 
 if __name__ == '__main__':
     main()
