c2dd2354224b3b8c507f2fc6e979a3dce9182f9b
Christopher Wingard <chris.wingard@oregonstate.edu>
2022-12-14 13:55:14 -0800

    Converting error flags from hex string to integer

--- cgsn_processing/process/proc_pwrsys.py
+++ cgsn_processing/process/proc_pwrsys.py
@@ -88,17 +88,18 @@ def main(argv=None):
             d = nc.createVariable(c, 'S6', ('time',))
             d.setncatts(PWRSYS[c])
             d[:] = df[c].values
-        elif c in ['override_flag', 'error_flag1', 'error_flag2', 'error_flag3']:
-            d = nc.createVariable(c, 'S8', ('time',))
-            d.setncatts(PWRSYS[c])
-            d[:] = df[c].values
+        # elif c in ['override_flag', 'error_flag1', 'error_flag2', 'error_flag3']:
+        #    d = nc.createVariable(c, 'S8', ('time',))
+        #    d.setncatts(PWRSYS[c])
+        #    d[:] = df[c].values
         else:
             # use the TimeSeries object to add the variables
-            ts.add_variable(c, df[c].values, fillvalue=-999999999, attributes=PWRSYS[c])
+            ts.add_variable(c, df[c].values, fillvalue=-9999999, attributes=PWRSYS[c])
 
     # synchronize the data with the netCDF file and close it
     nc.sync()
     nc.close()
+
 
 if __name__ == '__main__':
     main()
