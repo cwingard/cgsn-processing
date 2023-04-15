97462b2d223af6e496003838c229625a5d1aafa3
Christopher Wingard <chris.wingard@oregonstate.edu>
2023-04-15 09:47:25 -0700

    Add changes from orchard

--- utilities/processors/process_imm_adcp.sh
+++ utilities/processors/process_imm_adcp.sh
@@ -1,25 +1,26 @@
 #!/bin/bash
 #
 # Read the parsed ADCP data files from the CGSN Global Surface Mooring
-# Inductive Modem lines and create processed datasets available in NetCDF
-# formatted files for further processing and review.
+# Inductive Modem lines and create processed datasets available in
+# NetCDF formatted files for further processing and review.
 #
-# C. Wingard 2023-04-14
+# C. Wingard 2023-03-03
 
 # Parse the command line inputs
-if [ $# -ne 7 ]; then
+if [ $# -ne 8 ]; then
     echo "$0: required inputs are the platform and deployment names, the latitude and longitude, the ADCP"
-    echo " directory name, the deployment depth, and the name of the file to process."
+    echo " directory name, the ADCP serial number, the deployment depth, and the name of the file to process."
     echo ""
-    echo "     example: $0 gi01sumo D0004 59.9462 -39.4737 imm/adcp 500 adcp_20171010_111822.json"
+    echo "     example: $0 gi01sumo D00004 59.9462 -39.4737 imm/adcp 18000 600 20230303_122800.adcpt.json"
     exit 1
 fi
 PLATFORM=${1,,}
 DEPLOY=${2^^}
 LAT=$3; LON=$4
-ADCP=$5
-DEPTH=$6
-FILE=`basename $7`
+ADCP=${5,,}
+SERIAL=$6
+DEPTH=$7
+FILE=`basename $8`
 
 # Set the default directory paths and input/output sources
 DATA="/home/ooiuser/data"
@@ -29,9 +30,12 @@ if [ ! -d `dirname $OUT` ]; then
     mkdir -p `dirname $OUT`
 fi
 
+# set the default name of the calibration coefficients file
+COEFFS="adcp.configuration.json"
+
 # Process the file
 if [ -e $IN ]; then
     cd /home/ooiuser/code/cgsn-processing || exit
     python -m cgsn_processing.process.proc_imm_adcp -p $PLATFORM -d $DEPLOY -lt $LAT -lg $LON -dp $DEPTH \
-        -i $IN -o $OUT
+      -sn $SERIAL -cf $COEFFS -i $IN -o $OUT
 fi
