901f53ab3773a5467bdf667d9b0d018dd87cdab9
PaulWhelan-whoi <paul.whelan@whoi.edu>
2023-12-07 14:10:01 +0000

    converted to use xarray

--- cgsn_processing/process/configs/attr_ifcb.py
+++ cgsn_processing/process/configs/attr_ifcb.py
@@ -74,38 +74,33 @@ IFCB = {
 
     'sampleNumber': {
         'long_name': 'Sample Number of current IFCB deployment',
-        'standard_name': 'sample_number',
-        'processing_level': 'parsed',
+#        'units': '', deliberately left blank, no units for this value
         'comment': 'Sample number since beginning of IFCB deployment'
     },
 
     'sampleType': {
         'long_name': 'Sample type',
-        'standard_name': 'sample_type',
-        'processing_level': 'parsed',
+#        'units': '', deliberately left blank, no units for this value
         'comment': 'Type of sample being run: Normal, beads, '
     },
 
     'triggerCount': {
         'long_name': 'Trigger count',
-        'standard_name': 'trigger_count',
-        'processing_level': 'parsed',
+        'units': 'counts',
         'comment': 'Number of qualifying images detected in sample'
     },
 
     'roiCount': {
         'long_name': 'Image count',
-        'standard_name': 'roi_count',
-        'processing_level': 'parsed',
+        'units': 'counts',
         'comment': 'Count of raw optical images in sample'
     },
 
     'humidity': {
         'long_name': 'Relative humidity',
-        'standard_name': 'humidity',
+        'standard_name': 'relative_humidity',
         'units': 'percent',
         '_FillValue': FILL_INT,
-        'processing_level': 'parsed',
         'comment': 'Relative humidity'
     },
 
@@ -114,188 +109,147 @@ IFCB = {
         'standard_name': 'temperature',
         'units': 'degrees_Celsius',
         '_FillValue': FILL_INT,
-        'processing_level': 'parsed',
         'comment': 'Temperature in degrees celsius'
     },
 
     'runTime': {
         'long_name': 'Run time',
-        'standard_name': 'run_time',
         'units': 'seconds',
         '_FillValue': FILL_INT,
-        'processing_level': 'parsed',
         'comment': 'Run time of sample in seconds'
     },
 
     'inhibitTime': {
         'long_name': 'Inhibit time',
-        'standard_name': 'inhibit_time',
         'units': 'seconds',
         '_FillValue': FILL_INT,
-        'processing_level': 'parsed',
         'comment': 'Analysis time during which samples do not trigger'
     },
 
     'pump1State': {
         'long_name': 'Pump 1 state',
-        'standard_name': 'pump1_state',
         'units': 'boolean',
-        '_FillValue': FILL_INT,
-        'processing_level': 'parsed',
         'comment': 'Logical on/off state'
     },
 
     'pump2State': {
         'long_name': 'Pump 2 state',
-        'standard_name': 'pump2_state',
         'units': 'boolean',
-        '_FillValue': FILL_INT,
-        'processing_level': 'parsed',
-        'comment': 'Logical on/off value'
+        'long_name': 'Photomultiplier A high voltage',
+        'units': 'volts',
+        'comment': 'High voltage level at photomultiplier A'
     },
 
     'PMTAhighVoltage': {
         'long_name': 'Photomultiplier A high voltage',
-        'standard_name': 'pmta_high_voltage',
         'units': 'volts',
         '_FillValue': FILL_INT,
-        'processing_level': 'parsed',
         'comment': 'High voltage level at photomultiplier A'
     },
 
     'PMTBhighVoltage': {
-        'long_name': 'Photomultiplier A high voltage',
-        'standard_name': 'pmtb_high_voltage',
+        'long_name': 'Photomultiplier B high voltage',
         'units': 'volts',
         '_FillValue': FILL_INT,
-        'processing_level': 'parsed',
         'comment': 'High voltage level at photomultiplier B'
     },
 
     'Alt_FlashlampControlVoltage': {
         'long_name': 'Alternate flash lamp control voltage',
-        'standard_name': 'alt_flashlamp_control_voltage',
         'units': 'volts',
         '_FillValue': FILL_INT,
-        'processing_level': 'parsed',
         'comment': 'Control voltage at alternate flash lamp'
     },
 
     'pumpDriveVoltage': {
         'long_name': 'Pump drive voltage',
-        'standard_name': 'pump_drive_voltage',
         'units': 'volts',
         '_FillValue': FILL_INT,
-        'processing_level': 'parsed',
         'comment': 'Voltage at pump drive'
     },
 
     'altPMTAHighVoltage': {
         'long_name': 'Alternate photomultiplier A high voltage',
-        'standard_name': 'alt_pmta_high_voltage',
         'units': 'volts',
         '_FillValue': FILL_INT,
-        'processing_level': 'parsed',
         'comment': 'High voltage level at alternate photomultiplier A'
     },
 
     'altPMTBHighVoltage': {
         'long_name': 'Alternate photomultiplier B high voltage',
-        'standard_name': 'alt_pmtb_high_voltage',
         'units': 'volts',
         '_FillValue': FILL_INT,
-        'processing_level': 'parsed',
         'comment': 'High voltage level at alternate photomultiplier B'
     },
 
     'syringeSamplingSpeed': {
         'long_name': 'Syringe sampling speed',
-        'standard_name': 'syringe_sampling_speed',
         'units': 'min syringe-1',
         '_FillValue': FILL_INT,
         # '_FillValue': np.nan,
-        'processing_level': 'parsed',
         'comment': 'Syringe sampling speed in min/syringe'
     },
 
     'syringeOffset': {
         'long_name': 'Syringe offset',
-        'standard_name': 'syringe_offset',
-        'units': '?',
+#        'units': '', deliberately left blank, no units for this value
         '_FillValue': FILL_INT,
-        'processing_level': 'parsed',
         'comment': 'Offset in time? of syringe in ?'
     },
 
     'NumberSyringesToAutoRun': {
         'long_name': 'Number of syringes to auto-run',
-        'standard_name': 'number_syringes_to_autorun',
         'units': 'counts',
         '_FillValue': FILL_INT,
-        'processing_level': 'parsed',
         'comment': 'Count of syringes to automatically run'
     },
 
     'SyringeSampleVolume': {
         'long_name': 'Syringe sample volume',
-        'standard_name': 'syringe_sample_volume',
         'units': 'ml',
         '_FillValue': FILL_INT,
-        'processing_level': 'parsed',
         'comment': 'Sample volume of syringe in ml'
     },
 
     'altSyringeSampleVolume': {
         'long_name': 'Alternate syringe sample volume',
-        'standard_name': 'alt_syringe_sample_volume',
         'units': 'ml',
         '_FillValue': FILL_INT,
-        'processing_level': 'parsed',
         'comment': 'Sample volume of alternate syringe in ml'
     },
 
     'sampleVolume2skip': {
         'long_name': 'sample volume to skip',
-        'standard_name': 'sample_volume_to_skip',
         'units': 'ml',
         '_FillValue': FILL_INT,
-        'processing_level': 'parsed',
         'comment': 'Amount of sample volume to skip processing'
     },
 
     'focusMotorSmallStep_ms': {
         'long_name': 'Focus motor small step',
-        'standard_name': 'focus_motor_small_step_ms',
-        'units': '?',
         '_FillValue': FILL_INT,
-        'processing_level': 'parsed',
+#        'units': '', deliberately left blank, no units for this value
         'comment': 'Focus motor small step'
     },
 
     'focusMotorLargeStep_ms': {
         'long_name': 'Focus motor large step',
-        'standard_name': 'focus_motor_large_step_ms',
-        'units': '?',
+#        'units': '', deliberately left blank, no units for this value
         '_FillValue': FILL_INT,
-        'processing_level': 'parsed',
         'comment': 'Focus motor large step'
     },
 
-    'laserMotorSmallStep': {
+    'laserMotorSmallStep_ms': {
         'long_name': 'Laser motor small step',
-        'standard_name': 'laser_motor_small_step',
-        'units': '?',
+#        'units': '', deliberately left blank, no units for this value
         '_FillValue': FILL_INT,
-        'processing_level': 'parsed',
         'comment': 'Laser motor small step'
     },
 
-    'laserMotorLargeStep': {
+    'laserMotorLargeStep_ms': {
         'long_name': 'Laser motor large step',
-        'standard_name': 'laser_motor_large_step',
-        'units': '?',
+#        'units': '', deliberately left blank, no units for this value
         '_FillValue': FILL_INT,
-        'processing_level': 'parsed',
         'comment': 'Laser motor large step'
     }
 }
--- cgsn_processing/process/proc_ifcb_hdr.py
+++ cgsn_processing/process/proc_ifcb_hdr.py
@@ -8,14 +8,46 @@
 """
 import os
 import re
+import numpy as np
 
-from pocean.utils import dict_update
-from pocean.dsg.timeseries.om import OrthogonalMultidimensionalTimeseries as OMTs
+from collections.abc import Mapping
+import xarray as xr
 
-from cgsn_processing.process.common import inputs, json2df, df2omtdf
+from cgsn_processing.process.common import inputs, json2df, update_dataset, ENCODING
 from cgsn_processing.process.configs.attr_ifcb import IFCB
 
+def proc_ifcb(infile, platform, deployment, lat, lon, depth) :
+    """
+    Main IFCB processing function. Loads the JSON formatted parsed data. 
+    Filled variables are returned and the dataset
+    processing level attribute is set to "parsed".
 
+    :param infile: JSON formatted parsed data file
+    :param platform: Name of the mooring the instrument is mounted on.
+    :param deployment: Name of the deployment for the input data file.
+    :param lat: Latitude of the mooring deployment.
+    :param lon: Longitude of the mooring deployment.
+    :param depth: Depth of the platform the instrument is mounted on.
+
+
+    :return df: An xarray dataset with the processed IFCB data
+    """
+
+    # load the json data file and return a panda dataframe, adding a deployment depth and ID
+    df = json2df(infile)
+    if df.empty:
+        # there was no data in this file, ending early
+        return None
+
+    # create an xarray data set from the data frame
+    df = xr.Dataset.from_dataframe(df)
+
+    df['deploy_id'] = xr.Variable(('time',), np.repeat(deployment, len(df.time)).astype(str))
+    df = update_dataset(df, platform, deployment, lat, lon, [depth, depth, depth], IFCB)
+    df.attrs['processing_level'] = 'parsed'
+
+    return df
+    
 def main(argv=None):
     
     # load the input arguments
@@ -28,26 +60,9 @@ def main(argv=None):
     lon = args.longitude
     depth = args.depth
 
-    # load the json data file and return a panda dataframe, adding a deployment depth and ID
-    df = json2df(infile)
-    if df.empty:
-        # there was no data in this file, ending early
-        return None
-
-    df['depth'] = depth
-    df['deploy_id'] = deployment
-
-    # convert the dataframe to a format suitable for the pocean OMTs
-    df = df2omtdf(df, lat, lon, depth)
-
-    # add to the global attributes for the IFCB
-    attrs = IFCB
-    attrs['global'] = dict_update(attrs['global'], {
-        'comment': 'Mooring ID: {}-{}'.format(platform.upper(), re.sub('\D', '', deployment))
-    })
-
-    nc = OMTs.from_dataframe(df, outfile, attributes=attrs)
-    nc.close()
+    df = proc_ifcb( infile, platform, deployment, lat, lon, depth )
+    if df:
+        df.to_netcdf(outfile, mode='w', format='NETCDF4', engine='netcdf4', encoding=ENCODING)
     
 if __name__ == '__main__':
     main()
