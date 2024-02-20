81790ebd4de0c1d41f16686faebde460d87e3bf0
Christopher Wingard <chris.wingard@oregonstate.edu>
2024-02-20 11:23:58 -0800

    Add depth as an input

--- utilities/processors/process_co2pro.sh
+++ utilities/processors/process_co2pro.sh
@@ -7,18 +7,19 @@
 # C. Wingard 2024-02-20
 
 # Parse the command line inputs
-if [ $# -ne 6 ]; then
+if [ $# -ne 7 ]; then
     echo "$0: required inputs are the platform and deployment names, the latitude and longitude, the pCO2"
-    echo " directory name, and the name of the file to process."
+    echo " directory name, the deployment depth, and the name of the file to process."
     echo ""
-    echo "     example: $0 ce02shsm D00018 44.63929 -124.30404 nsif/pco2test 20161012.pco2test.json"
+    echo "     example: $0 ce02shsm D00018 44.63929 -124.30404 nsif/pco2test 7 20161012.pco2test.json"
     exit 1
 fi
 PLATFORM=${1,,}
 DEPLOY=${2^^}
 LAT=$3; LON=$4
 PCO2=${5,,}
-FILE=$(basename $6)
+DEPTH=$6
+FILE=$(basename $7)
 
 # Set the default directory paths and input/output sources
 DATA="/home/ooiuser/data"
@@ -31,6 +32,6 @@ fi
 # Process the file
 if [ -e $IN ]; then
     cd /home/ooiuser/code/cgsn-processing || exit
-    python -m cgsn_processing.process.proc_mopak -p $PLATFORM -d $DEPLOY -lt $LAT -lg $LON -dp 4 \
+    python -m cgsn_processing.process.proc_mopak -p $PLATFORM -d $DEPLOY -lt $LAT -lg $LON -dp $DEPTH \
       -i $IN -o $OUT || echo "Processing failed for $IN"
 fi
