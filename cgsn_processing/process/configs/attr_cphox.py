fd4b7765c9965ddf7b4bee188adbaa7cab522b52
Christopher Wingard <chris.wingard@oregonstate.edu>
2024-02-23 14:56:13 -0800

    Deep SeapHOx V2 processing module

---
+++ cgsn_processing/process/configs/attr_cphox.py
@@ -0,0 +1,172 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+@package cgsn_processing.process.configs.attr_co2pro
+@file cgsn_processing/process/configs/attr_co2pro.py
+@author Christopher Wingard
+@brief Attributes for the Pro-Oceanus pCO2-Pro CV variables
+"""
+CPHOX = {
+    # global attributes
+    'global': {
+        'title': 'Sea-Bird Electronics Deep SeapHOx V2 Data',
+        'summary': ('In-situ pH data from the Sea-Bird Electronics Deep SeapHOx V2 instrument. The SeapHOx is a '
+                    'combined CTD, dissolved oxygen and pH sensor.'),
+        'project': 'Ocean Observatories Initiative',
+        'institution': 'OOI Endurance Array',
+        'acknowledgement': 'National Science Foundation',
+        'references': 'http://oceanobservatories.org',
+        'creator_name': 'Ocean Observatories Initiative',
+        'creator_email': 'helpdesk@oceanobservatories.org',
+        'creator_url': 'http://oceanobservatories.org',
+        'featureType': 'timeSeries',
+        'cdm_data_type': 'Station',
+        'Conventions': 'CF-1.6'
+    },
+    'deploy_id': {
+        'long_name': 'Deployment ID',
+        'comment': ('Mooring deployment ID. Useful for differentiating data by deployment, '
+                    'allowing for overlapping deployments in the data sets.')
+    },
+    'station': {
+        'cf_role': 'timeseries_id',
+        'long_name': 'Station Identifier'
+    },
+    'time': {
+        'long_name': 'Time',
+        'standard_name': 'time',
+        'units': 'seconds since 1970-01-01T00:00:00.000Z',
+        'axis': 'T',
+        'calendar': 'gregorian',
+        'comment': 'Derived from the GPS referenced clock used by DCL data logger'
+    },
+    'lon': {
+        'long_name': 'Longitude',
+        'standard_name': 'longitude',
+        'units': 'degrees_east',
+        'axis': 'X',
+        'comment': ('Mooring deployment location, surveyed after deployment to determine the anchor location and '
+                    'the center of the watch circle.')
+    },
+    'lat': {
+        'long_name': 'Latitude',
+        'standard_name': 'latitude',
+        'units': 'degrees_north',
+        'axis': 'Y',
+        'comment': ('Mooring deployment location, surveyed after deployment to determine the anchor location and '
+                    'the center of the watch circle.')
+    },
+    'z': {
+        'long_name': 'Depth',
+        'standard_name': 'depth',
+        'units': 'm',
+        'comment': 'Instrument deployment depth',
+        'positive': 'down',
+        'axis': 'Z'
+    },
+    # variable attributes
+    'serial_number': {
+        'long_name': 'Serial Number',
+        # 'units': '',    # deliberately left blank, no units for this value,
+        'comment': 'Serial number of the SeapHOx instrument. Used to uniquely identify the instrument.'
+    },
+    'sensor_time': {
+        'long_name': 'Sensor Time',
+        'standard_name': 'time',
+        'units': 'seconds since 1970-01-01T00:00:00.000Z',
+        'comment': ('Internal SeapHOX clock date and time stamp, recorded when the instrument begins the measurement. '
+                    'It is expected that this value will drift from the true time by some amount over the course of '
+                    'a deployment. Cross-comparisons to other systems will be required to account for the offset '
+                    'and drift.'),
+    },
+    'sample_number': {
+        'long_name': 'Sample Number',
+        'comment': 'Sample number from the SeapHOx instrument.',
+        'units': 'count'
+    },
+    'error_flag': {
+        'long_name': 'Error Flag',
+        # 'units': '',    # deliberately left blank, no units for this value,
+        'comment': ('Error flag from the SeapHOx instrument. A value of 0 indicates no error. At this time, the '
+                    'meaning of other values is not known.'),
+    },
+    'temperature': {
+        'long_name': 'Sea Water Temperature',
+        'standard_name': 'sea_water_temperature',
+        'units': 'degrees_Celsius',
+        'comment': 'Sea water temperature is the in situ temperature of the sea water.',
+        'data_product_identifier': 'TEMPWAT_L1',
+    },
+    'seawater_ph': {
+        'long_name': 'Sea Water pH',
+        'standard_name': 'sea_water_ph_reported_on_total_scale',
+        'units': '1',
+        'comment': ('pH of the sea water, reported on the total scale. The pH is a measure of the acidity or '
+                    'basicity of the sea water, where a pH of 7 is neutral, a pH less than 7 is acidic, and a pH '
+                    'greater than 7 is basic.'),
+        'data_product_identifier': 'PHWATER_L2'
+    },
+    'external_reference': {
+        'long_name': 'External Reference Voltage',
+        'units': 'V',
+        'comment': ('External reference voltage from the SeapHOx instrument. This value is used to calculate the '
+                    'pH of the sea water.')
+    },
+    'pressure': {
+        'long_name': 'Sea Water Pressure',
+        'standard_name': 'sea_water_pressure_due_to_sea_water',
+        'units': 'dbar',
+        'comment': ('Seawater Pressure refers to the pressure exerted on a sensor in situ by the weight of the '
+                    'column of seawater above it. It is calculated by subtracting one standard atmosphere from the '
+                    'absolute pressure at the sensor to remove the weight of the atmosphere on top of the water '
+                    'column. The pressure at a sensor in situ provides a metric of the depth of that sensor.'),
+        'data_product_identifier': 'PRESWAT_L1'
+    },
+    'salinity': {
+        'long_name': 'Practical Salinity',
+        'standard_name': 'sea_water_practical_salinity',
+        'units': '1',
+        'comment': ('Salinity is generally defined as the concentration of dissolved salt in a parcel of sea water. '
+                    'Practical Salinity is a more specific unitless quantity calculated from the conductivity of '
+                    'sea water and adjusted for temperature and pressure. It is approximately equivalent to Absolute '
+                    'Salinity (the mass fraction of dissolved salt in sea water), but they are not interchangeable.'),
+        'data_product_identifier': 'PRACSAL_L2',
+        'ancillary_variables': 'conductivity, temperature, pressure'
+    },
+    'conductivity': {
+        'long_name': 'Sea Water Conductivity',
+        'standard_name': 'sea_water_electrical_conductivity',
+        'units': 'mS cm-1',
+        'comment': ('Sea water conductivity refers to the ability of seawater to conduct electricity. The presence '
+                    'of ions, such as salt, increases the electrical conducting ability of seawater. As such, '
+                    'conductivity can be used as a proxy for determining the quantity of salt in a sample of '
+                    'seawater.'),
+        'data_product_identifier': 'CONDWAT_L1'
+    },
+    'oxygen_molar_concentration': {
+        'long_name': 'Dissolved Oxygen Molar Concentration',
+        'standard_name': 'mole_concentration_of_dissolved_molecular_oxygen_in_sea_water',
+        'units': 'umol L-1',
+        'comment': ('Mole concentration of dissolved oxygen per unit volume, also known as Molarity, as measured by '
+                    'an optode oxygen sensor. Computed on-board the sensor using internal calibration coefficients.'),
+        'data_product_identifier': 'DOCONCS_L1'
+    },
+    'oxygen_concentration_per_kg': {
+        'long_name': 'Dissolved Oxygen per Unit Mass',
+        'standard_name': 'moles_of_oxygen_per_unit_mass_in_sea_water',
+        'units': 'umol kg-1',
+        'comment': ('Concentration of dissolved oxygen per unit mass in the sea water, adjusted to the potential '
+                    'density per directions outlined in the Sea-Bird Electronics SBE63 Manual.'),
+        'data_product_identifier': 'DOXYGEN_L2'
+    },
+    'internal_humidity': {
+        'long_name': 'Internal Humidity',
+        'units': 'percent',
+        'comment': 'Internal humidity of the SeapHOx instrument.'
+    },
+    'internal_temperature': {
+        'long_name': 'Internal Temperature',
+        'units': 'degree_Celsius',
+        'comment': 'Internal temperature of the SeapHOx instrument.'
+    }
+}
---
+++ cgsn_processing/process/proc_cphox.py
@@ -0,0 +1,100 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+@package cgsn_processing.process.proc_cphox
+@file cgsn_processing/process/proc_cphox.py
+@author Christopher Wingard
+@brief Creates a NetCDF dataset for the Sea-Bird Electronics Deep SeapHOx V2
+    data from the JSON formatted data
+"""
+import numpy as np
+import os
+import pandas as pd
+import xarray as xr
+
+from calendar import timegm
+from gsw import SA_from_SP, pt0_from_t, CT_from_pt, sigma0, z_from_p
+
+from cgsn_processing.process.common import ENCODING, inputs, json2df, update_dataset
+from cgsn_processing.process.configs.attr_cphox import CPHOX
+
+
+def proc_cphox(infile, platform, deployment, lat, lon, depth, **kwargs):
+    """
+    Processing function for the Sea-Bird Electronics Deep SeapHOx (combined
+    CTD, dissolved oxygen and pH sensor). Loads the JSON formatted parsed
+    data and saves the data to a NetCDF file.
+
+    :param infile: JSON formatted parsed data file
+    :param platform: Name of the mooring the instrument is mounted on.
+    :param deployment: Name of the deployment for the input data file.
+    :param lat: Latitude of the mooring deployment.
+    :param lon: Longitude of the mooring deployment.
+    :param depth: Depth of the platform relative to the sea surface.
+    :return cphox: xarray dataset with the processed SeapHOx data
+    """
+    # load the json data file as a pandas data frame
+    cphox = json2df(infile)
+    if cphox.empty:
+        # json data file was empty, exiting
+        return None
+
+    # convert SeapHOx date/time string to a pandas.Timestamp date/time object and then to a epoch time in seconds
+    utc = pd.to_datetime(cphox['sphox_date_time_string'], format='%Y-%m-%dT%H:%M:%S', utc=True)
+    epts = [timegm(t.timetuple()) for t in utc]  # calculate the epoch time as seconds since 1970-01-01 in UTC
+    cphox['sensor_time'] = epts
+
+    # drop the DCL date and time string, we no longer need it
+    cphox = cphox.drop(columns=['dcl_date_time_string', 'sphox_date_time_string'])
+
+    # reset the error code and serial number to integers
+    cphox['error_flag'] = cphox['error_flag'].astype(int)
+    cphox['serial_number'] = cphox['serial_number'].astype(int)
+
+    # convert the oxygen concentration from ml/l to umol/L and then to umol/kg per the SBE63 manual
+    SA = SA_from_SP(cphox['salinity'], cphox['pressure'], lon, lat)
+    pt0 = pt0_from_t(SA, cphox['temperature'], cphox['pressure'])
+    CT = CT_from_pt(SA, pt0)
+    sigma = sigma0(SA, CT)
+    cphox['oxygen_molar_concentration'] = cphox['oxygen_concentration'] * 44.6615  # convert from ml/l to umol/kg
+    cphox['oxygen_concentration_per_kg'] = cphox['oxygen_molar_concentration'] / sigma
+    cphox = cphox.drop(columns=['oxygen_concentration'])
+
+    # replace the deployment depth with the actual depth from the pressure sensor
+    depth = z_from_p(cphox['pressure'], lat)  # calculate the depth from the pressure
+    darray = [depth.mean(), depth.min(), depth.max()]
+
+    # create an xarray data set from the data frame
+    cphox = xr.Dataset.from_dataframe(cphox)
+
+    # assign/create needed dimensions, geo coordinates and update the metadata attributes for the data set
+    cphox['deploy_id'] = xr.Variable(('time',), np.repeat(deployment, len(cphox.time)).astype(str))
+    cphox = update_dataset(cphox, platform, deployment, lat, lon, darray, CPHOX)
+    return cphox
+
+
+def main(argv=None):
+    """
+    Command line function to process the SeapHOx data using the proc_cphox
+    function. Command line arguments are parsed and passed to the function.
+
+    :param argv: List of command line arguments
+    """
+    # load the input arguments
+    args = inputs(argv)
+    infile = os.path.abspath(args.infile)
+    outfile = os.path.abspath(args.outfile)
+    platform = args.platform
+    deployment = args.deployment
+    lat = args.latitude
+    lon = args.longitude
+    depth = args.depth
+
+    # process the CTDBP data and save the results to disk
+    cphox = proc_cphox(infile, platform, deployment, lat, lon, depth)
+    if cphox:
+        cphox.to_netcdf(outfile, mode='w', format='NETCDF4', engine='netcdf4', encoding=ENCODING)
+
+
+if __name__ == '__main__':
+    main()
