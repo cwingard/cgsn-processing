de79352ca46bb0e01312a49084870f4a1ecb4d30
Christopher Wingard <chris.wingard@oregonstate.edu>
2024-04-26 15:37:42 -0700

    Updating the last set of scripts for the new polling syntax

--- utilities/processors/process_hydgn.sh
+++ utilities/processors/process_hydgn.sh
@@ -5,32 +5,17 @@
 # processing and review.
 #
 # C. Wingard 2017-01-24
+# C. Wingard 2024-04-26 -- Updated to use the process_options.sh script to
+#                          parse the command line inputs
 
-# Parse the command line inputs
-if [ $# -ne 6 ]; then
-    echo "$0: required inputs are the platform and deployment names, the latitude and longitude, the HYDGN"
-    echo " directory name, and the name of the file to process."
-    echo ""
-    echo "     example: $0 ce02shsm D00004 44.63929 -124.30404 buoy/hydgn 20161012.hydgn.json"
-    exit 1
-fi
-PLATFORM=${1,,}
-DEPLOY=${2^^}
-LAT=$3; LON=$4
-HYDGN=${5,,}
-FILE=`basename $6`
-
-# Set the default directory paths and input/output sources
-
-DATA="/home/ooiuser/data"
-IN="$DATA/proc/$PLATFORM/$DEPLOY/$HYDGN/$FILE"
-OUT="$DATA/erddap/$PLATFORM/$DEPLOY/$HYDGN/${FILE%.json}.nc"
-if [ ! -d `dirname $OUT` ]; then
-    mkdir -p `dirname $OUT`
-fi
+# include the help function and parse the required and optional command line options
+DIR="${BASH_SOURCE%/*}"
+if [[ ! -d "$DIR" ]]; then DIR="$PWD"; fi
+source "$DIR/process_options.sh"
 
 # Process the file
 if [ -e $IN_FILE ]; then
-    cd /home/ooiuser/code/cgsn-processing
-    python -m cgsn_processing.process.proc_hydgn -p $PLATFORM -d $DEPLOY -lt $LAT -lg $LON -dp 0 -i $IN_FILE -o $OUT_FILE
+    cd /home/ooiuser/code/cgsn-processing || exit
+    python -m cgsn_processing.process.proc_hydgn -p $PLATFORM -d $DEPLOY -lt $LAT -lg $LON -dp $DEPTH \
+      -i $IN_FILE -o $OUT_FILE || echo "ERROR: Failed to process $IN_FILE"
 fi
