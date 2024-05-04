d48b76d4d7dcf9dd2406e9676628a06439601c5a
Christopher Wingard <chris.wingard@oregonstate.edu>
2024-05-04 10:14:33 -0700

    Additional adjustments to align with v4.11

--- cgsn_processing/process/proc_swnd.py
+++ cgsn_processing/process/proc_swnd.py
@@ -13,6 +13,9 @@ import xarray as xr
 from cgsn_processing.process.common import inputs, json2df, update_dataset, ENCODING
 from cgsn_processing.process.configs.attr_swnd import SWND
 
+# constant used by the ASIMET SWND v4.11 firmware processing
+PI2 = 6.283185
+
 
 def wind_binning(tbin):
     """
@@ -26,21 +29,24 @@ def wind_binning(tbin):
     # calculate the 1-minute simple averages for the bin
     avg = tbin.mean(dim='time')
 
-    # add the max wind speed for the bin
+    # add the min and max wind speeds for the bin
+    avg['wind_speed_min'] = tbin['wind_speed'].min(dim='time')
     avg['wind_speed_max'] = tbin['wind_speed'].max(dim='time')
 
     # replace the averaged heading and relative wind directions with the last valid value
     avg['heading'] = tbin['heading'][-1]
     avg['relative_direction'] = tbin['relative_direction'][-1]
 
-    # re-calculate the wind direction from the averaged eastward and northward wind components
-    avg['wind_direction'] = np.mod(np.degrees(np.arctan2(avg['eastward_wind_asimet'],
-                                                         avg['northward_wind_asimet'])), 360)
-
     # use the scalar-averaged wind speed and the wind direction calculated from vector averages to re-calculate the
     # eastward and northward wind components per directions provided by the NDBC (https://www.ndbc.noaa.gov/wndav.shtml)
-    avg['eastward_wind_ndbc'] = avg['wind_speed'] * np.sin(np.radians(avg['wind_direction']))
-    avg['northward_wind_ndbc'] = avg['wind_speed'] * np.cos(np.radians(avg['wind_direction']))
+    wind_direction = np.arctan2(avg['eastward_wind_asimet'], avg['northward_wind_asimet'])  # reported in radians
+    wind_direction = np.where(wind_direction < 0, wind_direction + PI2, wind_direction)  # 0 to 360 degrees, in radians
+    avg['eastward_wind_ndbc'] = avg['wind_speed'] * np.sin(wind_direction)
+    avg['northward_wind_ndbc'] = avg['wind_speed'] * np.cos(wind_direction)
+
+    # replace the averaged wind direction with one derived from the vector averages (convert radians to degrees)
+    avg['wind_direction'] = wind_direction * 57.29578  # convert to degrees (rad * (360 / 2 * pi))
+
     return avg
 
 
@@ -52,7 +58,16 @@ def proc_swnd(infile, platform, deployment, lat, lon, depth):
     later case, the METBK wind components are too low at higher wind speeds.
     This data should provide a better estimate of the wind speed and direction
     at higher wind speeds and should allow for a determination of why the
-    METBK reported values are biased low.
+    METBK reported values are biased low. Note, this processing follows the
+    steps outlined in the v4.11 firmware processing, but with some minor
+    modifications, while the METBK data set is using the v4.20 firmware.
+
+    For more information on the ASIMET system and the SWND module, see the
+    ASIMET website:
+
+        "https://www.whoi.edu/what-we-do/explore/instruments/\
+            instruments-sensors-samplers/\
+            air-sea-interaction-meteorology-the-asimet-system/"
 
     :param infile: JSON formatted parsed data file
     :param platform: Name of the mooring the instrument is mounted on.
@@ -80,35 +95,36 @@ def proc_swnd(infile, platform, deployment, lat, lon, depth):
     swnd = swnd.drop(columns=['u_axis_wind_speed', 'v_axis_wind_speed'])
 
     # convert the compass heading to radians
-    heading = swnd['heading'] * 0.0174533  # convert to radians
+    heading = swnd['heading'] * 0.0174533  # convert to radians (deg * (2 * pi / 360))
 
     # calculate the wind speed
     swnd['wind_speed'] = np.sqrt(swnd['eastward_wind_relative']**2 + swnd['northward_wind_relative']**2)
 
-    # calculate the relative wind direction
+    # calculate the relative wind direction, first correcting the northward wind component to avoid
+    # divide by zero errors
     swnd['northward_wind_relative'] = np.where(swnd['northward_wind_relative'] == 0, 0.00001,
-                                               swnd['northward_wind_relative'])  # avoid divide by zero errors
+                                               swnd['northward_wind_relative'])
     direction = np.arctan2(swnd['eastward_wind_relative'], swnd['northward_wind_relative'])
 
     # convert to 0 to 360 degrees, but leave in radians for further calculations
-    direction = np.where(direction < 0, direction + 2*np.pi, direction)
+    direction = np.where(direction < 0, direction + PI2, direction)
 
     # if the wind speed is less than 0.05 m/s, set the relative wind direction to a NaN and then forward
     # fill the NaNs with the last valid value (per the Gill WindMasterII manual) -- this differs from the
-    # METBK processing, which does not make this adjustment
+    # v4.11 processing, which does not make this adjustment
     direction = np.where(swnd['wind_speed'] < 0.05, np.nan, direction)
 
     # calculate the wind direction relative to magnetic north (using the compass heading and relative wind direction)
     wind_direction = direction + heading
-    wind_direction = np.where(wind_direction > 2 * np.pi, wind_direction - 2 * np.pi, wind_direction)
+    wind_direction = np.where(wind_direction > PI2, wind_direction - PI2, wind_direction)
 
     # now compute the eastward and northward wind components
     swnd['eastward_wind_asimet'] = swnd['wind_speed'] * np.sin(wind_direction)
     swnd['northward_wind_asimet'] = swnd['wind_speed'] * np.cos(wind_direction)
 
     # add the wind direction and relative wind direction (converted to degrees) to the data frame
-    swnd['wind_direction'] = np.degrees(wind_direction)
-    swnd['relative_direction'] = np.degrees(direction)
+    swnd['wind_direction'] = wind_direction * 57.29578  # convert to degrees (rad * (360 / 2 * pi))
+    swnd['relative_direction'] = direction * 57.29578   # convert to degrees (rad * (360 / 2 * pi))
 
     # create an xarray data set from the data frame
     swnd = xr.Dataset.from_dataframe(swnd)
@@ -117,7 +133,7 @@ def proc_swnd(infile, platform, deployment, lat, lon, depth):
     swnd['time'] = swnd.time + np.timedelta64(30, 's')
 
     # resample the data to 1-minute bins using the wind_binning function defined above
-    swnd = swnd.resample(time='1Min', label='left').map(wind_binning)
+    swnd = swnd.resample(time='1Min', label='left', skipna=True).map(wind_binning)
 
     # assign/create needed dimensions, geo coordinates and update the metadata attributes for the data set
     swnd['deploy_id'] = xr.Variable(('time',), np.repeat(deployment, len(swnd.time)).astype(str))
