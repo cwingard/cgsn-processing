4e1316a4fb970ff7a1d3a220c612d4a7abdcb6cc
PaulWhelan-whoi <paul.whelan@whoi.edu>
2024-09-18 16:17:27 +0000

    process scripts for recent processors (ifcb_adc, adcpu, xeos)

---
+++ utilities/processors/process_ifcb_adc.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#
+# Read the parsed IFCB ADC data files from the CGSN Coastal Surface Moorings and
+# create processed datasets available in NetCDF formatted files.
+#
+# P. Whelan 2024-09-18 -- Copied from process_ifcb.sh
+
+# include the help function and parse the required and optional command line options
+DIR="${BASH_SOURCE%/*}"
+if [[ ! -d "$DIR" ]]; then DIR="$PWD"; fi
+source "$DIR/process_options.sh"
+
+# Process the file
+if [ -e $IN_FILE ]; then
+    cd /home/ooiuser/code/cgsn-processing || exit
+    python -m cgsn_processing.process.proc_ifcb_adc -p $PLATFORM -d $DEPLOY -lt $LAT -lg $LON -dp $DEPTH \
+      -i $IN_FILE -o $OUT_FILE || echo "ERROR: Failed to process $IN_FILE"
+fi
