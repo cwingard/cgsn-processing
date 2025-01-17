dd8e8892ccd66e866c6d3f7b10976a86be6a9714
Christopher Wingard <cwingard@coas.oregonstate.edu>
2025-01-17 13:27:45 -0800

    Still testing and developing

---
+++ cgsn_processing/process/templates/keys.py
@@ -0,0 +1,23 @@
+# JSON keys
+PART = 'part'
+ASSEMBLY_PARTS = 'assembly_parts'
+CONFIGURATION_VALUES = 'configuration_values'
+PARENT_ASSEMBLY_PART_ID = 'parent_assembly_part_id'
+PARENT_ASSEMBLY_PART_URL = 'parent_assembly_part_url'
+PART_NAME = 'part_name'
+ASSEMBLY_PART_ID = 'assembly_part_id'
+ASSEMBLY_PART_URL = 'assembly_part_url'
+INVENTORY_SERIAL_NUMBER = 'inventory_serial_number'
+PARENT = 'parent'
+CONFIG_DEFAULT_EVENTS = 'config_default_events'
+
+# config keys
+COMPONENT_NAME = 'Component Name'
+INSTANCE_ON_SUBASSEMBLY = 'Instance on Subassembly'
+PARENT_CPU = 'Parent CPU'
+DATA_SOURCE_LOG_IDENTIFIER = 'Data Source Log Identifier'
+
+# new keys
+SUBASSEMBLIES = ['mfn', 'nsif', 'buoy']
+CPUS = ['stc', 'cpm', 'dcl']
+
